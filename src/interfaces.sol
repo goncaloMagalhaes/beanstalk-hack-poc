c99e5c8f3fc493639b1e2453ce754ea79a589bf9
goncaloMagalhaes <gonmrm@protonmail.com>
2022-12-22 16:42:53 +0000

    Clean IBeanStalk and ICurvePool interfaces

--- src/interfaces.sol
+++ src/interfaces.sol
@@ -14,83 +14,9 @@ interface IAaveLendingPool {
 }
 
 interface ICurvePool {
-    function A() external view returns (uint256 out);
-
     function add_liquidity(uint256[2] memory amounts, uint256 min_mint_amount) external;
-
     function add_liquidity(uint256[3] memory amounts, uint256 min_mint_amount) external;
-
     function add_liquidity(uint256[4] memory amounts, uint256 min_mint_amount) external;
-
-    function admin_fee() external view returns (uint256 out);
-
-    function balances(uint256 arg0) external view returns (uint256 out);
-
-    function calc_token_amount(
-        uint256[] memory amounts, 
-        bool is_deposit
-    ) external view returns (uint256 lp_tokens);
-
-    function coins(int128 arg0) external view returns (address out);
-    function coins(uint256 arg0) external view returns (address out);
-
-    function underlying_coins(int128 arg0) external view returns (address out);
-    function underlying_coins(uint256 arg0) external view returns (address out);
-
-    function exchange(
-        int128 i,
-        int128 j,
-        uint256 dx,
-        uint256 min_dy
-    ) external;
-    function exchange(
-        int128 i,
-        int128 j,
-        uint256 dx,
-        uint256 min_dy,
-        address receiver
-    ) external returns (uint256);
-
-    function exchange_underlying(
-        int128 i,
-        int128 j,
-        uint256 dx,
-        uint256 min_dy
-    ) external;
-    function exchange_underlying(
-        address pool,
-        int128 i,
-        int128 j,
-        uint256 dx,
-        uint256 min_dy
-    ) external;
-
-    function fee() external view returns (uint256 out);
-
-    function future_A() external view returns (uint256 out);
-
-    function future_fee() external view returns (uint256 out);
-
-    function future_admin_fee() external view returns (uint256 out);
-
-    function get_dy(
-        int128 i,
-        int128 j,
-        uint256 dx
-    ) external view returns (uint256);
-
-    function get_dy_underlying(
-        int128 i,
-        int128 j,
-        uint256 dx
-    ) external view returns (uint256);
-
-    function get_virtual_price() external view returns (uint256 out);
-
-    function remove_liquidity(
-        uint256 token_amount, 
-        uint256[3] memory min_amounts
-    ) external returns (uint256[3] memory);
 
     function remove_liquidity_imbalance(
         uint256[3] memory amounts,
@@ -111,19 +37,6 @@ interface IBeanStalk {
 
     function deposit(address token, uint256 amount) external;
 
-    function vote(uint32 bip) external;
-
-    function bip(uint32 bipId) external view returns (
-        address,
-        uint32,
-        uint32,
-        bool,
-        int256,
-        uint128,
-        uint256,
-        uint256
-    );
-
     struct FacetCut {
         address facetAddress;
         uint8 action;
@@ -136,6 +49,4 @@ interface IBeanStalk {
         bytes calldata _calldata,
         uint8 _pauseOrUnpause
     ) external;
-
-    function numberOfBips() external view returns (uint32);
 }
